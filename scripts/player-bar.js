43db0289cb6b063092959463401517e9a129e690
psopher <phsophe@gmail.com>
2017-12-01 21:19:03 +0100

    checkpoint 11 assignment

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,6 +1,6 @@
 {
   $('button#play-pause').on('click', function() {
-     player.playPause();
+     helper.playPauseAndUpdate();
      $(this).attr('playState', player.playState);
   });
 
@@ -12,7 +12,8 @@
     if (nextSongIndex >= album.songs.length) { return; }
 
     const nextSong = album.songs[nextSongIndex];
-    player.playPause(nextSong);
+
+    helper.playPauseAndUpdate(nextSong);
   });
 
   $('button#previous').on('click', function() {
@@ -23,9 +24,10 @@
     if (previousSongIndex < 0) { return; }
 
     const previousSong = album.songs[previousSongIndex];
-    player.playPause(previousSong);
+    helper.playPauseAndUpdate(previousSong);
   });
 
+  //Time control
   $('#time-control input').on('input', function (event) {
     player.skipTo(event.target.value);
   });
@@ -35,7 +37,14 @@
     const currentTime = player.getTime();
     const duration = player.getDuration();
     const percent = (currentTime / duration) * 100;
-    $('#time-control .current-time').text( currentTime );
+    $('#time-control .current-time').text(player.prettyTime(currentTime));
     $('#time-control input').val(percent);
+
+    $('#time-control .total-time').text(player.prettyTime(duration));
   }, 1000);
+
+  //Volume Control
+  $('#volume-control input').on('input', function (event) {
+    player.setVolume(event.target.value);
+  });
 }
